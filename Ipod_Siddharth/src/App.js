c3c70d30bbfdf294f37f1d01bfc6bcbe672f9489
Siddharth <siddharth.singhvi08@gmail.com>
2021-07-03 15:08:14 +0530

    ipod-Reactjs

---
+++ Ipod_Siddharth/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Ipod from './Ipod';
+// main App Component
+function App() {
+  return (
+    <div className="App">
+      <Ipod />
+    </div>
+  );
+}
+
+export default App;
